3d9b7800447596165ebee1e1b71540e97e6d0a2c
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2015-10-28 10:37:24 -0400

    Started outlining RTG_simulation.m

--- RTG_simulation.m
+++ RTG_simulation.m
@@ -1,1 +1,23 @@
-function RTG_simulation()
+function mass = RTG_simulation(duration, power)
+
+%% initialize parameters
+InitParams;
+params.missionDuration = duration;
+params.powerThreshold = power;
+
+
+%% Setup end case
+% options = odeset('EndOfLife', @event_EOL)
+% 
+%     function event_EOL(t, 
+%         
+%     end
+
+%% ODE FX HERE
+
+
+
+%%%% BROOOOOOOOOOKEN
+mass = ;
+
+end
