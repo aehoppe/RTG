c33132606134e19a52d6a8fe3c13c3d241fbadde
Taylor Sheneman <tsheneman@olin.edu>
2015-10-28 11:06:45 -0400

    Added flow function. Should be compatible with ode45.

---
+++ RTGFlows.m
@@ -0,0 +1,25 @@
+% Flow function for RTG heat flow model. Your omegas are too high.
+
+function res = RTGFlows(t, X)
+
+% unpack input vector
+activeFuelMass = X(1); % First element: Pu-238 mass stock
+rtgHeat = X(2); % Second element: RTG heat energy stock
+
+% define flows
+dmdt = -ln(2) * (1 / params.puHalfLife) * activeFuelMass; % mass flow
+
+dQdt = dmdt * params.puEnergyPerKg + ... % radioactive decay energy
+    - params.emissivity * params.stefanBoltzman * ...
+    (energyToTemp(rtgHeat, params.puMass, puSpecificHeat))^4; % radiation
+
+% pack results (mass; heat energy)
+res = [dmdt; dQdt];
+
+end
+
+function temp = energyToTemp (energy, mass, specificHeat)
+
+    temp = energy / (mass * specificHeat);
+    
+end
